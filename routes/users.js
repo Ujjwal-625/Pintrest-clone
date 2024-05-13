038e42cf2c506e1695b93e1e185377e58c8aabe5
sujal7878 <93308791+sujal7878@users.noreply.github.com>
2024-05-13 16:05:12 +0530

    Update and delete image

--- routes/users.js
+++ routes/users.js
@@ -20,7 +20,7 @@ const userSchema=mongoose.Schema({
   },
   dp:{
     type:String,
-    default:"https://www.shutterstock.com/shutterstock/photos/561472912/display_1500/stock-vector-account-add-contact-create-friend-new-user-icon-561472912.jpg"
+    default:"https://imgs.search.brave.com/gV6Xy99WsNTWpgT2KUNxopKhP45u8QMrrL2DGi5HYxg/rs:fit:500:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzAyLzE1Lzg0LzQz/LzM2MF9GXzIxNTg0/NDMyNV90dFg5WWlJ/SXllYVI3TmU2RWFM/TGpNQW15NEd2UEM2/OS5qcGc",
   },
   fullname:{
     type:String,
